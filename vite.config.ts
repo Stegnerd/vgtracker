e55653dec01a24f4b91392b06c1e7b8bf0967f6b
Dalton Stegner <dmstegner@gmail.com>
2024-03-02 14:27:53 -0500

    setup vitest (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
@@ -23,5 +24,10 @@ export default defineConfig(async () => ({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
+  },
+  test: {
+    /* use global to avoid globals imports (describe, test, expect): */
+    globals: true,
+    environment: "jsdom"
   }
 }));
