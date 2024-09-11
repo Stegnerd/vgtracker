67ba35b4269e5a9cd99c29b405e4c9723a0ef24b
Dalton Stegner <dmstegner@gmail.com>
2024-09-10 20:41:17 -0400

    theme working in composable

--- src/components/AppTopbar.vue
+++ src/components/AppTopbar.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
   import { useDialog } from "primevue/usedialog";
   import { ref } from "vue";
+  import { useLayout } from "../composables/layout";
   import AppThemeConfigurator from "./AppThemeConfigurator.vue";
+
+  const { toggleDarkMode } = useLayout();
 
   const dialog = useDialog();
 
   const isDarkMode = ref(true);
 
-  function toggleDarkMode() {
-    const element = document.querySelector("html");
-    element!.classList.toggle("app-dark");
-    isDarkMode.value = !isDarkMode.value;
-  }
+  // function toggleDarkMode() {
+  //   const element = document.querySelector("html");
+  //   element!.classList.toggle("app-dark");
+  //   isDarkMode.value = !isDarkMode.value;
+  // }
 
   function openThemeDialog() {
     dialog.open(AppThemeConfigurator, {
